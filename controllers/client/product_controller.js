a26e241fd208a8aa9aaf449c214d0d13d99bb6c9
Kiet Tuan <theonlytruth25012005@gmail.com>
2025-02-13 17:22:37 +0700

    Hien thi san pham

--- controllers/client/product_controller.js
+++ controllers/client/product_controller.js
@@ -1,5 +1,19 @@
-module.exports.index= (req,res) => {
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req,res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
+  console.log(products);
+
+  const newProducts = products.map(item => {
+    item.priceNew = (item.price - item.price*(item.discountPercentage/100)).toFixed(0);
+    return item;
+  });
+
   res.render("client/pages/products/index",{
-    pageTitle:"Danh sach san pham"
+    pageTitle:"Danh sach san pham",
+    products: newProducts
   });
 };
