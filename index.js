394308b859529a0282ce54835221162cd7b2e41b
Kiet Tuan <theonlytruth25012005@gmail.com>
2025-02-13 16:26:01 +0700

    Thiet lap ket noi database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 
+const database = require("./config/database");
+
+
 const route = require("./routes/client/index.route");
+database.connect();
 
 const app = express();
 const port = process.env.PORT;
@@ -9,6 +13,8 @@ const port = process.env.PORT;
 
 app.set("views","./views");
 app.set("view engine","pug");
+
+app.use(express.static("public"));
 
 // Route
 route(app);
