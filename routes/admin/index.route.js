1fd5a5c3eebe706da8298c2a9147ad5b775d3ae0
Kiet Tuan <theonlytruth25012005@gmail.com>
2025-02-16 23:10:24 +0700

    Thiet lap route cho product

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,8 @@
 const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
 const PATH_ADMIN = require("../../config/system");
 
 module.exports = (app) => {
   app.use(`${PATH_ADMIN.prefixAdmin}/dashboard`,dashboardRoutes);
+  app.use(`${PATH_ADMIN.prefixAdmin}/products`,productRoutes);
 }
