7d3e9258e6b5e07f997d98db3e759e3aca48b61c
Kiet Tuan <theonlytruth25012005@gmail.com>
2025-02-18 17:00:43 +0700

    xong tinh nang loc

---
+++ public/admin/js/script.js
@@ -0,0 +1,24 @@
+// Button Status
+const buttonStatus = document.querySelectorAll("[button-status]");
+if(buttonStatus.length > 0){
+  let url = new URL(window.location.href);
+
+  buttonStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+
+      if(status){
+        url.searchParams.set("status",status);
+      }
+      else{
+        url.searchParams.delete("status");
+      }
+      // console.log(url.href);
+
+      // Redirect
+      window.location.href = url.href;
+
+    });
+  });
+};
+// End Button Status
