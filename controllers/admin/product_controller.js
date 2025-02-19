c56a903b9d02deb99d5b870b3b2755c6993110d1
Kiet Tuan <theonlytruth25012005@gmail.com>
2025-02-19 15:58:05 +0700

    tinh nang tim kiem

--- controllers/admin/product_controller.js
+++ controllers/admin/product_controller.js
@@ -32,11 +32,19 @@ module.exports.index = async (req,res) => {
   }
 
   let find = {
-    deleted: false
+    deleted: false,
   };
   if(req.query.status){
     find.status = req.query.status;
   }
+
+  let keyword = "";
+  if(req.query.keyword){
+    keyword = req.query.keyword.replace(/\s+/g, "");
+    const regex = new RegExp(keyword,"i");
+    find.title = regex;
+  }
+  
 
   const products = await Product.find(find);
 
@@ -44,6 +52,7 @@ module.exports.index = async (req,res) => {
   res.render("admin/pages/products/index",{
     pageTitle: "Trang san pham",
     products: products,
-    filterStatus: filterStatus
+    filterStatus: filterStatus,
+    keyword: keyword
   });
 }
