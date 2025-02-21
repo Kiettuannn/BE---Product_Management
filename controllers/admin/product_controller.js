ff247cd59ca8df7e5e85b8cf2b97aff037d266bb
Kiet Tuan <theonlytruth25012005@gmail.com>
2025-02-22 03:49:03 +0700

    toi uu pagination

--- controllers/admin/product_controller.js
+++ controllers/admin/product_controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 // [GET] /admin/products
 module.exports.index = async (req,res) => {
 
@@ -21,24 +22,18 @@ module.exports.index = async (req,res) => {
   }
 
   // Pagination
-  let objectPagination = {
-    currentPage: 1,
-    limitItem: 2
-  }
-  if(req.query.page){
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-  objectPagination.skip = (objectPagination.currentPage - 1)*objectPagination.limitItem;
   const countProducts = await Product.collection.count(find);
-  const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
-
-  objectPagination.totalPage = totalPage;
-  // End Pagination
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItem: 2
+    },
+    req.query,
+    countProducts
+  );
 
 
   const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
-
-
   res.render("admin/pages/products/index",{
     pageTitle: "Trang san pham",
     products: products,
