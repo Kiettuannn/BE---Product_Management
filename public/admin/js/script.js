cd52680ae341651777e618d00188a0c72d3643fe
KietTuan <theonlytruth25012005@gmail.com>
2025-07-22 16:06:57 +0700

    Add image preview for product creation form
    Introduced an image preview feature when uploading a product thumbnail in the admin product creation form. Updated CSS for image preview styling, added JavaScript to handle image file input and preview, and modified the Pug template to include the necessary attributes and preview image element.

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,16 +1,15 @@
 // Button Status
 const buttonStatus = document.querySelectorAll("[button-status]");
-if(buttonStatus.length > 0){
+if (buttonStatus.length > 0) {
   let url = new URL(window.location.href);
 
   buttonStatus.forEach(button => {
     button.addEventListener("click", () => {
       const status = button.getAttribute("button-status");
 
-      if(status){
-        url.searchParams.set("status",status);
-      }
-      else{
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
         url.searchParams.delete("status");
       }
       // console.log(url.href);
@@ -26,35 +25,34 @@ if(buttonStatus.length > 0){
 
 // Form search
 const formSearch = document.querySelector("#form-search");
-if(formSearch){
+if (formSearch) {
   let url = new URL(window.location.href);
 
-  formSearch.addEventListener("submit",(e) => {
+  formSearch.addEventListener("submit", (e) => {
     e.preventDefault();
     const keyword = e.target.elements.keyword.value;
-    if(keyword){
-      url.searchParams.set("keyword",keyword);
-    }
-    else{
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
       url.searchParams.delete("keyword");
     }
     window.location.href = url.href;
   });
-} 
+}
 // EndForm search 
 
 // Pagination 
 const buttonPagination = document.querySelectorAll("[button-pagination]");
 
 
-if(buttonPagination.length > 0){
+if (buttonPagination.length > 0) {
   let url = new URL(window.location.href);
 
-  buttonPagination.forEach(button =>{
+  buttonPagination.forEach(button => {
     button.addEventListener("click", () => {
       const page = button.getAttribute("button-pagination");
-      if(page){
-       url.searchParams.set("page",page);
+      if (page) {
+        url.searchParams.set("page", page);
       }
       window.location.href = url.href;
     });
@@ -64,17 +62,16 @@ if(buttonPagination.length > 0){
 
 // Checkbox multi 
 const checkBoxMulti = document.querySelector("[checkbox-multi]");
-if(checkBoxMulti){
+if (checkBoxMulti) {
   const inputcheckAll = checkBoxMulti.querySelector("input[name='checkall']");
   const inputId = checkBoxMulti.querySelectorAll("input[name='id']");
 
   inputcheckAll.addEventListener("click", () => {
-    if(inputcheckAll.checked){
+    if (inputcheckAll.checked) {
       inputId.forEach(input => {
         input.checked = true;
       })
-    }
-    else{
+    } else {
       inputId.forEach(input => {
         input.checked = false;
       })
@@ -84,10 +81,9 @@ if(checkBoxMulti){
     input.addEventListener("click", () => {
       const countChecked = checkBoxMulti.querySelectorAll("input[name='id']:checked").length;
 
-      if(countChecked == inputId.length){
+      if (countChecked == inputId.length) {
         inputcheckAll.checked = true;
-      }
-      else{
+      } else {
         inputcheckAll.checked = false;
       }
     })
@@ -97,7 +93,7 @@ if(checkBoxMulti){
 
 // Form change multi 
 const formChangeMulti = document.querySelector("[form-change-multi]");
-if(formChangeMulti){
+if (formChangeMulti) {
   formChangeMulti.addEventListener("submit", (e) => {
     e.preventDefault();
     const checkBoxMulti = document.querySelector("[checkbox-multi]");
@@ -106,33 +102,31 @@ if(formChangeMulti){
     // Lay ra loai cua lua chon de ap dung
 
     const typeChange = e.target.elements.type.value; // type: la name cua select option
-    if(typeChange == "delete-all"){
+    if (typeChange == "delete-all") {
       const isConfirm = confirm("Ban co chac muon xoa khong ?");
 
-      if(!isConfirm){
+      if (!isConfirm) {
         return;
       }
     }
 
-    if(inputsChecked.length > 0){
+    if (inputsChecked.length > 0) {
       let ids = [];
       const inputIds = formChangeMulti.querySelector("input[name='ids']");
       inputsChecked.forEach(input => {
         const id = input.value;
 
-        if(typeChange == "change-position"){
+        if (typeChange == "change-position") {
           const position = input.closest("tr").querySelector("input[name='position']").value;
           ids.push(`${id}-${position}`);
-        }
-        else{
+        } else {
           ids.push(id);
         }
       })
       inputIds.value = ids.join(", ")
 
       formChangeMulti.submit();
-    }
-    else{
+    } else {
       alert("Chon it nhat 1 ban ghi");
     }
   });
@@ -142,7 +136,7 @@ if(formChangeMulti){
 
 // Show alert
 const showAlert = document.querySelector("[show-alert]")
-if(showAlert){
+if (showAlert) {
   const time = parseInt(showAlert.getAttribute("data-time"));
   const closeAlert = showAlert.querySelector("[close-alert]");
 
@@ -156,4 +150,18 @@ if(showAlert){
 // End Show alert
 
 
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
 
+  uploadImageInput.addEventListener("change", (e) => {
+    // console.log(e);
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End Upload Image
