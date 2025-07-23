6fb7933a47eb1c809611e47e076f036ba1fe5cdb
KietTuan <theonlytruth25012005@gmail.com>
2025-07-23 14:41:42 +0700

    Add product detail page and route (client)
    Implemented product detail view and route for individual products using slug. Updated controller to handle detail logic, modified product listing to link to detail pages, and added basic styling changes. Also added a new product image to uploads.

--- controllers/client/product_controller.js
+++ controllers/client/product_controller.js
@@ -1,20 +1,39 @@
-
 const Product = require("../../models/product.model")
 
 // [GET] /products
-module.exports.index = async (req,res) => {
+module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
     deleted: false
-  }).sort({position: "asc"});
+  }).sort({
+    position: "asc"
+  });
 
   const newProducts = products.map(item => {
-    item.priceNew = (item.price - item.price*(item.discountPercentage/100)).toFixed(0);
+    item.priceNew = (item.price - item.price * (item.discountPercentage / 100)).toFixed(0);
     return item;
   });
 
-  res.render("client/pages/products/index",{
-    pageTitle:"Danh sach san pham",
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sach san pham",
     products: newProducts
   });
 };
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      slug: req.params.slug,
+      deleted: false,
+      status: "active"
+    }
+    const product = await Product.findOne(find);
+    res.render("client/pages/products/detail", {
+      pageTitle: product.title,
+      product: product
+    });
+  } catch (error) {
+    res.redirect("/products");
+  }
+}
