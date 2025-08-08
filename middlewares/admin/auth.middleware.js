97f6f9e8b043ea4a2ada7c9298c8a4daab3ae40d
KietTuan <theonlytruth25012005@gmail.com>
2025-08-08 12:24:02 +0700

    Implement role-based permissions in admin views
    Added role and permissions checks to admin middleware and views. Navigation, actions, and page access are now conditionally rendered based on the user's role permissions, improving security and access control throughout the admin panel.

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,4 +1,5 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 const systemConfig = require("../../config/system");
 
 module.exports.requireAuth = async (req, res, next) => {
@@ -13,7 +14,15 @@ module.exports.requireAuth = async (req, res, next) => {
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
+      const role = await Role.findOne({
+        _id: user.role_id,
+        deleted: false
+      }).select("title permissions");
+
+      res.locals.user = user;
+      res.locals.role = role;
       next();
+
     }
   }
 }
